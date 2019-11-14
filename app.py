7a1032b818b4c422e7f259e2810bbd93ac3e9f2a
Carter Van Ekeren <vanek015@umn.edu>
2019-11-13 19:51:31 -0600

    Add select word length element to form

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import requests
 import itertools
 from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Regexp
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import Optional, Regexp
 import re
 
 class WordForm(FlaskForm):
     avail_letters = StringField("Letters", validators= [
         Regexp(r'^[a-z]+$', message="must contain letters only")
     ])
+    word_length = SelectField("Word Length", validators=[Optional()],
+    choices=[("", '--')] + [(i,i) for i in range(3,11)])
     submit = SubmitField("Go")
 
 
